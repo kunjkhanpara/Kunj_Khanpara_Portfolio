6eba0233cf32d8fcb501d765f0183f756e8b70a5
kunjkhanpara <kunjpatel4647@gmail.com>
2025-01-09 14:25:01 +0530

    Add full portfolio project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,14 @@
 import React from "react";
-import ProjectCard from "./ProjectCard";
 import "./Projects.css";
 
 function Projects() {
   return (
     <div className="projects">
       <h2>My Projects</h2>
-      <div className="project-list">
+      <div className="maintenance-message">
+        <p>Page is Under Maintenance</p>
+      </div>
+      {/* <div className="project-list">
         <ProjectCard
           title="Weather Application"
           description="Built using React and OpenWeatherMap API."
@@ -17,7 +19,7 @@ function Projects() {
           description="A resume builder app using React and Node.js."
           link="https://github.com/kunjkhanpara/Digital-Resume-Builder"
         />
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,23 +1,20 @@
-import React from "react";
-import "./Resume.css";
+import React from 'react';
+import './Resume.css';
 
-function Resume() {
+const Resume = () => {
   return (
     <div className="resume-container">
-      <h2>My Resume</h2>
+      <div className="image-container">
+        <img src={require("./4.jpg")} alt="Resume" className="zoomable-image" />
+      </div>
       <a
         href="https://drive.usercontent.google.com/u/0/uc?id=1w-kCHk8eM7D0R-skdD4pUwTlI68LVyaR&export=download"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="download-button"
+        className="download-btn"
       >
         Download Resume
       </a>
-      <div className="image-container">
-        <img src={require("./4.jpg")} alt="Resume Preview" className="resume-image" />
-      </div>
     </div>
   );
-}
+};
 
 export default Resume;
