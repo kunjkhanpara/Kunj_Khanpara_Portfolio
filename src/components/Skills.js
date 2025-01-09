2c2a45b51e602c98748793b1bfb37fe7072231b4
kunjkhanpara <kunjpatel4647@gmail.com>
2025-01-09 11:52:43 +0530

    Add full portfolio project

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,42 @@
 import React from "react";
 import "./Skills.css";
+import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaNodeJs, FaDatabase } from "react-icons/fa";
 
 function Skills() {
-  const skills = ["HTML", "CSS", "JavaScript", "React.js", "Node.js", "MySQL"];
   return (
     <div className="skills">
-      <h2>My Skills</h2>
-      <ul className="skills-list">
-        {skills.map((skill, index) => (
-          <li key={index} className="skill-item">{skill}</li>
-        ))}
-      </ul>
+      <div className="skills-header">
+        <h2>My Skills</h2>
+        <p>
+        I specialize in building responsive and user-friendly web applications using modern tools and technologies. Below are the core skills I focus on
+        </p>
+      </div>
+      <div className="skills-icons">
+        <div className="icon-container">
+          <FaHtml5 className="icon" />
+          <span className="icon-title">HTML</span>
+        </div>
+        <div className="icon-container">
+          <FaCss3Alt className="icon" />
+          <span className="icon-title">CSS</span>
+        </div>
+        <div className="icon-container">
+          <FaJs className="icon" />
+          <span className="icon-title">JavaScript</span>
+        </div>
+        <div className="icon-container">
+          <FaReact className="icon" />
+          <span className="icon-title">React.js</span>
+        </div>
+        <div className="icon-container">
+          <FaNodeJs className="icon" />
+          <span className="icon-title">Node.js</span>
+        </div>
+        <div className="icon-container">
+          <FaDatabase className="icon" />
+          <span className="icon-title">MongoDB</span>
+        </div>
+      </div>
     </div>
   );
 }
