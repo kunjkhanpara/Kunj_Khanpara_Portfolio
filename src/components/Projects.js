46c7c62139adc6dd2da5caf43b88f391f8e8a5d9
kunjkhanpara <kunjpatel4647@gmail.com>
2025-01-10 08:37:21 +0530

    Add full portfolio project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,15 +1,12 @@
 import React from "react";
+import ProjectCard from "./ProjectCard";
 import "./Projects.css";
 
 function Projects() {
   return (
     <div className="projects">
       <h2>My Projects</h2>
-      <div className="maintenance-message">
-        <p>Page is Under Maintenance</p>
-        <div className="loading-spinner"></div>
-      </div>
-      {/* <div className="project-list">
+      <div className="project-list">
         <ProjectCard
           title="Weather Application"
           description="Built using React and OpenWeatherMap API."
@@ -20,7 +17,22 @@ function Projects() {
           description="A resume builder app using React and Node.js."
           link="https://github.com/kunjkhanpara/Digital-Resume-Builder"
         />
-      </div> */}
+         <ProjectCard
+          title="Digital Resume Builder"
+          description="A resume builder app using React and Node.js."
+          link="https://github.com/kunjkhanpara/Digital-Resume-Builder"
+        />
+         <ProjectCard
+          title="Digital Resume Builder"
+          description="A resume builder app using React and Node.js."
+          link="https://github.com/kunjkhanpara/Digital-Resume-Builder"
+        />
+         <ProjectCard
+          title="Digital Resume Builder"
+          description="A resume builder app using React and Node.js."
+          link="https://github.com/kunjkhanpara/Digital-Resume-Builder"
+        />
+      </div>
     </div>
   );
 }
